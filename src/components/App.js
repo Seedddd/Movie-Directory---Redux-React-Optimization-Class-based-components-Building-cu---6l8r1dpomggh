9851247ecc985a0b4b1751be38517ea64137fe43
satyesh tiwari <107918066+Seedddd@users.noreply.github.com>
2023-01-03 23:08:52 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,24 @@
-import React from "react";
+import React, {useState}from "react";
 import "../styles/App.css";
-import MovieProvider from "./MovieContext";
 import Movie from "./Movie";
 import MoviesList from "./MoviesList";
 
 const App = () => {
+
+  const [val, setVal] = useState([])
+  function call(val){
+    setVal(val)
+  }
+  // console.log(val.length===0)
+  
   return (
-    <MovieProvider>
-      <div id="main">
-        <h1>Movie List</h1>
-        <MoviesList />
-        <Movie />
-      </div>
-    </MovieProvider>
+    <div id="main">
+      <h1>Movie List</h1>
+      <MoviesList function={call}/>
+      {
+        val.length===0 ? <div id="movies-banner">Invalid Id</div>: <Movie Movie_director={val.director} Movie_year={val.year}  Movie_title={val.title}/>
+      }
+    </div>
   );
 };
 
